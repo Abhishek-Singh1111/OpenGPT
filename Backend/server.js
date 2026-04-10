aef89969695eedbe2486d2fe14ea370810bc9d14
Abhishek <Abhishek-Singh1111>
2026-04-10 16:31:39 +0530

    adding port.env

--- Backend/server.js
+++ Backend/server.js
@@ -30,37 +30,11 @@ const connectDB = async () => {
 
 const startServer = async () => {
     await connectDB();
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0', () => {
         console.log(`server running on ${PORT}`);
     });
 };
 
 startServer();
 
-
-// app.post("/test", async (req, res) => {
-//     const options = {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-//         },
-//         body: JSON.stringify({
-//             model: "gpt-4o-mini",
-//             messages: [{
-//                 role: "user",
-//                 content: req.body.message
-//             }]
-//         })
-//     };
-
-//     try {
-//         const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-//         const data = await response.json();
-//         //console.log(data.choices[0].message.content); //reply
-//         res.send(data.choices[0].message.content);
-//     } catch(err) {
-//         console.log(err);
-//     }
-// });
 
